3499069babb0223ddfdfab6b565780e3c576f605
vKxni <74613145+vKxni@users.noreply.github.com>
2022-06-25 10:02:28 +0200

    fixed unused functions

--- R/bot.R
+++ R/bot.R
@@ -6,21 +6,12 @@ library(jsonlite)
 
 source(here::here("secrets.R")) # environment for bot token (secret)
 
-BOT_CHANNEL_ID = "server-channel-id"
 
 bot <- DiscordrBot$new(token)
 
 bot$register_event_handler("MESSAGE_CREATE", function(msg){
-  # avoid handling your own messages
-  if (msg$author$id == "your-discord-id"){
-    return()
-  }
-  # guard to work only in test channel
-  if (msg$channel_id != BOT_CHANNEL_ID){
-    return()
-  }
   message <- paste("Hi,", msg$author$username, "Simple reply to your message: '", 
-                   msg$content, "' (from your favourite discord bot")
+                   msg$content, "' (from your favourite discord bot lol")
   send_message(message, msg$channel_id, bot)
 })
 
